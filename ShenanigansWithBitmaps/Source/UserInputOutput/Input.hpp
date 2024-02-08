d0c694d0c031fbe40e527e5b349d18e7fccbe328
Matt <144626797+Im-Bee@users.noreply.github.com>
2024-02-08 06:56:14 +0100

    Delete ShenanigansWithBitmaps/Source/UserInputOutput directory

--- ShenanigansWithBitmaps/Source/UserInputOutput/Input.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace SWInputOutput
-{
-    std::wstring InputString(const std::wstring& message, const bool& suppressSetMessage = false);
-}
--- ShenanigansWithBitmaps/Source/UserInputOutput/Output.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define PROMPT_MAX_SIZE 512
-
-namespace SWInputOutput
-{
-    void OutputTextFromResources(const int& id);
-}
